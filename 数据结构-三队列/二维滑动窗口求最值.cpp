f17e00af6aed9d254075d0b3020d0e76fd4c4ccc
miracle-555 <13235595638@163.com>
2025-07-23 22:45:55 +0800

    数据结构-三队列

---
+++ 数据结构-三队列/二维滑动窗口求最值.cpp
@@ -0,0 +1,84 @@
+/*
+单调队列+二维滑动窗口
+n和m代表矩阵v的行数和列数，a和b代表滑动窗口的大小
+最后输出所有滑动窗口的最大值与最小值的乘积之和
+对998244353取模
+*/
+
+/*
+示例输入:
+2 3 1 2
+1 2 3
+4 5 6
+示例输出:
+58
+*/
+
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+const ll MOD = 998244353;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    ll n, m, a, b, res = 0;
+    cin >> n >> m >> a >> b;
+    ll v[n][m];
+    for (ll i = 0; i < n; i++) for (ll j = 0; j < m; j++) cin >> v[i][j];
+
+    ll v_subM[n][m - b + 1], v_subm[n][m - b + 1];  // 第一道压缩，分别储存列上连续b项的最值
+    deque<ll> q;
+    for (ll i = 0; i < n; i++){
+        ll j = 0;
+        for (; j < b - 1; j++){
+            while (!q.empty() && v[i][j] >= v[i][q.back()]) q.pop_back();
+            q.push_back(j);
+        }
+        for (; j < m; j++){
+            while (!q.empty() && v[i][j] >= v[i][q.back()]) q.pop_back();
+            q.push_back(j);
+            if (q.front() <= j - b) q.pop_front();
+            v_subM[i][j - b + 1] = v[i][q.front()];
+        }
+        q.clear();
+        j = 0;
+        for (; j < b - 1; j++){
+            while (!q.empty() && v[i][j] <= v[i][q.back()]) q.pop_back();
+            q.push_back(j);
+        }
+        for (; j < m; j++){
+            while (!q.empty() && v[i][j] <= v[i][q.back()]) q.pop_back();
+            q.push_back(j);
+            if (q.front() <= j - b) q.pop_front();
+            v_subm[i][j - b + 1] = v[i][q.front()];
+        }
+        q.clear();
+    }
+
+    deque<ll> q1, q2;    // 第二道压缩，分别代表最大值和最小值的
+    for (ll j = 0; j < m - b + 1; j++){
+        ll i = 0;
+        for (; i < a - 1; i++){
+            while (!q1.empty() && v_subM[i][j] >= v_subM[q1.back()][j]) q1.pop_back();
+            q1.push_back(i);
+            while (!q2.empty() && v_subm[i][j] <= v_subm[q2.back()][j]) q2.pop_back();
+            q2.push_back(i);
+        }
+        for (; i < n; i++){
+            while (!q1.empty() && v_subM[i][j] >= v_subM[q1.back()][j]) q1.pop_back();
+            q1.push_back(i);
+            if (q1.front() <= i - a) q1.pop_front();
+            while (!q2.empty() && v_subm[i][j] <= v_subm[q2.back()][j]) q2.pop_back();
+            q2.push_back(i);
+            if (q2.front() <= i - a) q2.pop_front();
+            res = (res + (v_subM[q1.front()][j] % MOD) * (v_subm[q2.front()][j] % MOD)) % MOD;
+        }
+        q1.clear();
+        q2.clear();
+    }
+    
+    cout << res;
+    return 0;
+}
---
+++ 数据结构-三队列/优先队列.cpp
@@ -0,0 +1,69 @@
+/*
+优先队列+链表
+一次操作: 找到最小元素并删去，然后把其两侧的元素都加上删去的元素的大小
+n代表数列大小，k代表操作次数
+技巧: 变化一个元素相当于直接删去变化的元素并加上变化后的元素
+*/
+
+/*
+示例输入:
+5 3
+1 4 2 8 7
+示例输出:
+17 7
+*/
+
+
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    ll n, k;
+    cin >> n >> k;
+    ll round = n - k;   // 最后剩余的个数
+    ll a[n + 1], l[n + 1], r[n + 1];  // l和r指的是a[i]所对应的数的左右索引，链表实现；+1是为了后面方便找head
+    r[0] = 1;    // 找head
+    for (ll i = 1; i <= n; i++){
+        cin >> a[i];
+        r[i] = i + 1;
+        l[i] = i - 1;
+    }
+    
+    priority_queue<pair<ll, ll>> q;  // <值，索引>，默认大根堆
+    for (ll i = 1; i <= n; i++) q.push({-a[i], -i});  // 借负数实现小根堆
+
+    while (k--){
+        auto tem = q.top(); q.pop();  // 这个时候的最小值可能是还没来得及删除的元素
+        while (-tem.first != a[-tem.second]){   // 值与当前实际a中的不同，就是应该删除的
+            tem = q.top(); q.pop();   // 删除多余的元素
+        }
+        ll v = -tem.first, id = -tem.second;
+
+        if (l[id] >= 1){     // 如果左边有元素才能操作，避免越界
+            r[l[id]] = r[id];
+            if (v != 0){
+                a[l[id]] += v;
+                q.push({-a[l[id]], -l[id]});  // v=0时不需要变化
+            }
+        }
+        if (r[id] <= n){    // 如果右边有元素
+            l[r[id]] = l[id];
+            if (v != 0){
+                a[r[id]] += v;
+                q.push({-a[r[id]], -r[id]});
+            }
+        }
+        if (l[id] == 0) r[0] = r[id];  // 单独提出来是为了找head
+    }
+    
+    ll head = r[0];
+    while (round--){
+        cout << a[head] << " ";
+        head = r[head];
+    }
+    return 0;
+}
